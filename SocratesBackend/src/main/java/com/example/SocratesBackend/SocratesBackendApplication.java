278ba5597caec2a59f39f87e9c1312efc14b6542
Edgar Yamill Parada Duarte <104601278+EdgarParada03@users.noreply.github.com>
2025-04-16 11:21:00 -0500

    Cargue Backend base

---
+++ SocratesBackend/src/main/java/com/example/SocratesBackend/SocratesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.SocratesBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocratesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocratesBackendApplication.class, args);
+	}
+
+}
---
+++ SocratesBackend/src/test/java/com/example/SocratesBackend/SocratesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SocratesBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocratesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
