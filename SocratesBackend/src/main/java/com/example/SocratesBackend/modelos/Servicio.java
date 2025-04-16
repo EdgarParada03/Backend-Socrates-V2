056d556a7f499fb66f0f9296913042c03e81b0ad
Edgar Parada <edgaryamillparadaduarte@gmail.com>
2025-04-16 12:25:34 -0500

    Cree el modelo de Servicio

--- SocratesBackend/src/main/java/com/example/SocratesBackend/modelos/Servicio.java
+++ SocratesBackend/src/main/java/com/example/SocratesBackend/modelos/Servicio.java
@@ -1,4 +1,118 @@
 package com.example.SocratesBackend.modelos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Servicio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "fecha_servicio", nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy", timezone = "UTC")
+    private LocalDate fechaServicio;
+
+    @Column(name = "descripcion", nullable = false)
+    private String descripcion;
+
+    @Column(name = "hora_servicio", nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm:ss")
+    private LocalTime horaServicio;
+
+    @Column(name = "estado", nullable = false)
+    private String estado;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "tipo_plan_id", nullable = false)
+    private TipoPlan tipoPlan;
+
+    @ManyToOne
+    @JoinColumn(name = "tecnico_id")
+    private Empleado tecnico;
+
+    @Column(name = "fecha_registro", nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy", timezone = "UTC")
+    private LocalDate fechaRegistro;
+
+    public Servicio() {
+    }
+
+    public Servicio(long id, LocalDate fechaServicio, String descripcion, LocalTime horaServicio, String estado, TipoPlan tipoPlan, Empleado tecnico, LocalDate fechaRegistro) {
+        this.id = id;
+        this.fechaServicio = fechaServicio;
+        this.descripcion = descripcion;
+        this.horaServicio = horaServicio;
+        this.estado = estado;
+        this.tipoPlan = tipoPlan;
+        this.tecnico = tecnico;
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public LocalDate getFechaServicio() {
+        return fechaServicio;
+    }
+
+    public void setFechaServicio(LocalDate fechaServicio) {
+        this.fechaServicio = fechaServicio;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public LocalTime getHoraServicio() {
+        return horaServicio;
+    }
+
+    public void setHoraServicio(LocalTime horaServicio) {
+        this.horaServicio = horaServicio;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public TipoPlan getTipoPlan() {
+        return tipoPlan;
+    }
+
+    public void setTipoPlan(TipoPlan tipoPlan) {
+        this.tipoPlan = tipoPlan;
+    }
+
+    public Empleado getTecnico() {
+        return tecnico;
+    }
+
+    public void setTecnico(Empleado tecnico) {
+        this.tecnico = tecnico;
+    }
+
+    public LocalDate getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(LocalDate fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
 }
