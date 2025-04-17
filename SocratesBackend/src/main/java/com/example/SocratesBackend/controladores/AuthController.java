2fd2706a6be062afa103a14dc20d04f2855a279d
jose sandoval <luisvalr120@gmail.com>
2025-04-17 09:44:49 -0500

    hice login

---
+++ SocratesBackend/src/main/java/com/example/SocratesBackend/controladores/AuthController.java
@@ -0,0 +1,22 @@
+package com.example.SocratesBackend.controladores;
+
+import com.example.SocratesBackend.servicios.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@CrossOrigin(origins = {"*"})
+@RequestMapping("/api/v1")
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/login")
+    public Map<String, String> login(@RequestParam String numeroDocumento, @RequestParam String password) {
+        return authService.authenticate(numeroDocumento, password);
+    }
+
+}
---
+++ SocratesBackend/src/main/java/com/example/SocratesBackend/servicios/AuthService.java
@@ -0,0 +1,36 @@
+package com.example.SocratesBackend.servicios;
+
+
+import com.example.SocratesBackend.modelos.Empleado;
+import com.example.SocratesBackend.modelos.Persona;
+import com.example.SocratesBackend.repositorios.EmpleadoRepository;
+import com.example.SocratesBackend.repositorios.PersonaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    private EmpleadoRepository empleadoRepository;
+
+    public Map<String, String> authenticate(String codigoEmpleado, String password) {
+        Map<String, String> response = new HashMap<>();
+        Optional<Empleado> empleado = empleadoRepository.findByCodigoEmpleado(codigoEmpleado);
+
+        if (empleado.isPresent() && empleado.get().getCodigoEmpleado().equals(password)) {
+            Empleado usuario = empleado.get();
+            response.put("message", "Login successful");
+            response.put("userType", usuario.getCargo().toLowerCase()); // administrador o secretaria
+        } else {
+            response.put("message", "Invalid credentials");
+        }
+
+        return response;
+    }
+
+}
