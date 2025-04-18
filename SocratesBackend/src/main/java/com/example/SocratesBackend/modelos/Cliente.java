792dba651357e0c96a1d0b2c50e87f160e3983d4
CRISTIAN <cristianestrada1609@gmail.com>
2025-04-18 11:40:50 -0500

    update

--- SocratesBackend/src/main/java/com/example/SocratesBackend/modelos/Cliente.java
+++ SocratesBackend/src/main/java/com/example/SocratesBackend/modelos/Cliente.java
@@ -1,6 +1,8 @@
 package com.example.SocratesBackend.modelos;
 
 import jakarta.persistence.*;
+
+import java.time.LocalDate;
 import java.util.Date;
 
 @Entity
@@ -9,24 +11,24 @@ public class Cliente extends Persona {
 
     @Temporal(TemporalType.DATE)
     @Column(name = "fecha_registro")
-    private Date fechaRegistro;
+    private LocalDate fechaRegistro;
 
     @Column(name = "tipo_cliente")
     private String tipoCliente;
 
     public Cliente() {}
 
-    public Cliente(Long id, String primerNombre, String segundoNombre, String primerApellido, String segundoApellido, String tipoIdentificacion, String numeroIdentificacion, String sexo, String correoElectronico, String telefono, Date fechaNacimiento, String lugarResidencia, String direccionCasa, String barrio, boolean estado, Date fechaRegistro, String tipoCliente) {
+    public Cliente(Long id, String primerNombre, String segundoNombre, String primerApellido, String segundoApellido, String tipoIdentificacion, String numeroIdentificacion, String sexo, String correoElectronico, String telefono, Date fechaNacimiento, String lugarResidencia, String direccionCasa, String barrio, boolean estado, LocalDate fechaRegistro, String tipoCliente) {
         super(id, primerNombre, segundoNombre, primerApellido, segundoApellido, tipoIdentificacion, numeroIdentificacion, sexo, correoElectronico, telefono, fechaNacimiento, lugarResidencia, direccionCasa, barrio, estado);
         this.fechaRegistro = fechaRegistro;
         this.tipoCliente = tipoCliente;
     }
 
-    public Date getFechaRegistro() {
+    public LocalDate getFechaRegistro() {
         return fechaRegistro;
     }
 
-    public void setFechaRegistro(Date fechaRegistro) {
+    public void setFechaRegistro(LocalDate fechaRegistro) {
         this.fechaRegistro = fechaRegistro;
     }
 
